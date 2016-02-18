377176dde9d838c73161711a0412f17f986da84a
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-02-17 19:53:26 -0500

    Clone dotmatrix git repository

--- provision.sh
+++ provision.sh
@@ -7,10 +7,27 @@ function log {
   echo "####################################";
 }
 
+function run_as_vagrant {
+  bash_command=$1;
+  sudo su vagrant -c "${bash_command}";
+}
+
 function apt_get_install {
   package=$1;
   log "install package = ${package}";
   sudo apt-get install -y ${package};
+}
+
+function git_clone {
+  project=$1;
+  folder="projects/${project}";
+  run_as_vagrant "mkdir -p ${folder}";
+  if [ ! -d "${folder}/.git/" ]; then
+    echo "git clone = ${project}"
+    git clone https://github.com/${project}.git ${folder};
+  else
+    echo "git already cloned = ${project}";
+  fi
 }
 
 log "update apt-get";
@@ -18,3 +35,5 @@ sudo apt-get update;
 
 apt_get_install "htop";
 apt_get_install "git";
+
+git_clone "hashrocket/dotmatrix";
